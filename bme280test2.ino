68877eddbe7ed588b40cdb10b2b860c006baae4d
Andrew Hill <andrew.2.hill@ucdenver.edu>
2016-11-12 17:24:10 -0700

    Added Arduino Code

---
+++ bme280test2.ino
@@ -0,0 +1,53 @@
+/***************************************************************************
+  This is a library for the BME280 humidity, temperature & pressure sensor
+
+  Designed specifically to work with the Adafruit BME280 Breakout
+  ----> http://www.adafruit.com/products/2650
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
+//Adafruit_BME280 bme(BME_CS); // hardware SPI
+//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
+byte buff[3];
+void setup() {
+  Serial.begin(9600);
+
+  if (!bme.begin()) {
+    Serial.println("Can't connect");
+    while (1);
+  } else {
+    Serial.println("Connected");
+  }
+
+}
+
+void loop() {
+    buff[0] = 'F';
+    word test = 27;
+    buff[1] = test/256;
+    buff[2] = test%256;
+    Serial.write(buff,3);
+    delay(5000);
+}
