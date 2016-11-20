4c6076ff5fdf69822275bead7dae82bb36287778
AmeenSassi <ameen_sassi@yahoo.com>
2016-11-19 18:23:28 -0700

    GUITooy

--- src/GUIMain.java
+++ src/GUIMain.java
@@ -8,7 +8,7 @@ public class GUIMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("NewGUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("NewGUI2.fxml"));
         primaryStage.setTitle("Hard-Weather");
         primaryStage.setScene(new Scene(root, 1000, 750));
         primaryStage.show();
@@ -17,4 +17,6 @@ public class GUIMain extends Application {
     public static void main(String[] args) {
         Application.launch(GUIMain.class, (java.lang.String[])null);;
     }
+
+
 }
