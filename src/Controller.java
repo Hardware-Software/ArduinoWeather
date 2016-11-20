bc0dbad51833eba50f082a0e6a955a9bcc61c093
Andrew <96ahill@gmail.com>
2016-11-19 18:19:52 -0700

    Controller.java now runs on a timer, Added arduino sketch code.

--- src/Controller.java
+++ src/Controller.java
@@ -1,14 +1,20 @@
 
+import java.awt.event.*;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import javafx.event.*;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
-public class Controller implements  Initializable {
+import javax.swing.*;
+
+public class Controller implements  Initializable, BufferReadyEvent {
 
     @FXML
     private Button tempButton;
@@ -26,36 +32,80 @@ public class Controller implements  Initializable {
     private Text pressText;
     @FXML
     private Text lightText;
-
+    private LinkedBlockingQueue lq;
+    private SerialComm port;
     @Override
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-
+        lq = new LinkedBlockingQueue();
+        port = new SerialComm("COM3",lq);
+        port.addListener(this);
+        ActionListener timer = new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e){
+                port.send('A');
+            }
+        };
+        Timer tm = new Timer(1000, timer);
+        tm.setInitialDelay(3000);
+        tm.start();
         tempButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                tempText.setText("Text");
+                port.send('T');
             }
         });
 
         humidButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-               humidText.setText("Text");
+                port.send('H');
             }
         });
 
         pressButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                pressText.setText("Text");
+                port.send('P');
             }
         });
 
         lightButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                lightText.setText("Text");
+                port.send('L');
             }
         });
+    }
+
+    @Override
+    public void bufferReady(char request){
+        Packet data = new Packet(0,0,0);
+        if(!lq.isEmpty()){
+            data = (Packet)lq.remove();
+            switch (request) {
+                case 'T':{
+                    tempText.setText(data.temperature + "C");
+                    break;
+                }
+                case 'P':{
+                    pressText.setText((data.pressure/100.0F)+ "hPa");
+                    break;
+                }
+                case 'L':{
+                    lightText.setText("Daytime/Nighttime");
+                    break;
+                }
+                case 'H':{
+                    humidText.setText(data.humidity + "RH");
+                    break;
+                }
+                case 'A': {
+                    tempText.setText(data.temperature + "C");
+                    pressText.setText((data.pressure/100.0F)+ "hPa");
+                    lightText.setText("Daytime/Nighttime");
+                    humidText.setText(data.humidity + "RH");
+                }
+            }
+        }
     }
 }
